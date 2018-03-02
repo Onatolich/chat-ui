d68ecd5601b64738a984f0dae3bfb2fbe09c8bd7
Eugene Tsakh <onatolich@gmail.com>
2018-03-03 00:33:34 +0200

    Create AvatarsService

---
+++ src/services/AvatarsService.js
@@ -0,0 +1,19 @@
+const AVATARS_ROOT = 'https://spotim-demo-chat-server.herokuapp.com/avatars/';
+
+const avatars = [
+  '001-snorlax.png',
+  '002-psyduck.png',
+  '003-pikachu.png',
+  '004-jigglypuff.png',
+  '005-bullbasaur.png',
+].map(avatar => `${AVATARS_ROOT}${avatar}`);
+
+export default {
+  getAll() {
+    return avatars;
+  },
+
+  getRandom() {
+    return avatars[Math.floor(Math.random() * avatars.length)];
+  },
+};
