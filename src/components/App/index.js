1a872f5813fcb07361027cafea8f94810cc00955
Eugene Tsakh <onatolich@gmail.com>
2018-03-02 22:21:08 +0200

    Create App structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import Header from '../Header';
+import MessagesList from '../MessagesList';
+import CreateMessageArea from '../CreateMessageArea';
+import './App.scss';
 
 export default function App() {
   return (
-    <div className="App">Hello, World!</div>
+    <MuiThemeProvider>
+      <div className="App">
+        <Paper className="App__Container">
+          <Header />
+          <MessagesList />
+          <CreateMessageArea />
+        </Paper>
+      </div>
+    </MuiThemeProvider>
   );
 }
