a2b121a3e3f4ada5d09f70b8be64457b8e3129ac
Eugene Tsakh <onatolich@gmail.com>
2018-03-05 16:05:12 +0200

    Update persistent avatar e2e test description

--- cypress/integration/avatar-is-persistent.spec.js
+++ cypress/integration/avatar-is-persistent.spec.js
@@ -1,6 +1,6 @@
 import fragments from '../fragments';
 
-describe('As a user I should have predefined avatar', () => {
+describe('As a user I should have persistent avatar', () => {
   let storedAvatar;
 
   it('Given I open the chat url', () => {
